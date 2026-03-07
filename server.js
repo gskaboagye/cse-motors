470224861858bb9d7e5b61bda067a48105afa2c6
gskaboagye <gskaboagye@gmail.com>
2026-03-07 17:59:24 +0000

    comit

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express")
 const path = require("path")
+const expressLayouts = require("express-ejs-layouts")
 
 const app = express()
 
 // View engine
 app.set("view engine", "ejs")
 
+// Layout configuration
+app.use(expressLayouts)
+app.set("layout", "layouts/main")
+
 // Static files
 app.use(express.static(path.join(__dirname, "public")))
 
 // Route for homepage
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index", {
+    title: "CSE Motors"
+  })
 })
 
 // Port
