c3b37edcdc5c71d06ef01d2e18f092f94282a408
gskaboagye <gskaboagye@gmail.com>
2026-03-27 00:18:11 +0000

    push

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
