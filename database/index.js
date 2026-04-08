1372d178ee497aa835db6ed0eb668ec23e1b4c3d
gskaboagye <gskaboagye@gmail.com>
2026-04-08 21:26:18 +0000

    commit

--- database/index.js
+++ database/index.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg")
 require("dotenv").config()
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD || ""),
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
